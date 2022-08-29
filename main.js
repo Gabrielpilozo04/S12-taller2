d5a93b129963002bc1b05cf5e24b3067be219a30
Gabrielpilozo04 <112340054+Gabrielpilozo04@users.noreply.github.com>
2022-08-28 20:15:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const contentido = document.getElementById("contentido")
+
+let datos="Practica de HTML, CSS Y JS";
+contenido.innerHTML= "<h2>"+ datos + "</h2>";
+
+
+let Nombre = "Gabriel Pilozo Narvaez";
+
+const color_rojo="#FF0000"
+let resultado = Nombre + Nombre;
+
+alert(resultado);
+
+Nombre= "Ingenieria en Software, 3er Semestre, Curso A2"
+alert(Nombre)
